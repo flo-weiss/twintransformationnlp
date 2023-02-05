1a70096111ad1c5671f87e1d67fa3eded48696df
flo-weiss <florian.weiss@fim-rc.de>
2023-02-05 17:36:50 +0100

    erste version des skripts

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# twintransformationnlp
+# Anleitung
+
+Damit das Skript läuft sollten die folgenden Anforderungen erfüllt werden:
+- Input CSV sollte mindestens die Spalte "content" enthalten
+- Path Variablen anpassen
+- Vorher pandas per pip3 installieren: pip3 install pandas
+- Vorher sPACY model importieren: python3 -m spacy download en_core_web_sm
+
+Für Tokenization einfach die Methoden aufrufen. Entsprechende txt files mit tokenized Inhalten werden am konfigurierten
+Pfad erstellt.
+
+Für Erstellung einzelner txt files:
+1) Erstelle csv mit gleich entweder positiven oder negativen Ergebnissen
+2) Konfiguriere csv-Pfad
+3) Konfiguriere folder-Pfad (am besten Ordner erstellen ("pos" oder "neg") und diesen Pfad einfügen)
---
+++ main.py
@@ -0,0 +1,102 @@
+import csv
+import spacy
+import pandas
+
+#TODO: diese Pfadangaben sollten über das git laufen und nicht lokal liegen
+path = '/home/florian/Documents/'
+path_csv = '/home/florian/Documents/test.csv'
+path_folder = '/home/florian/Documents/ST/'
+list_sentence = []
+list_word = []
+
+def main():
+    nlp = spacy.load("en_core_web_sm")
+
+    tokenize_word(nlp)
+    #print(str(list_word))
+
+    tokenize_sentence(nlp)
+    #print(str(list_sentence))
+
+    #Mode for creation of txt files can be "sentence" or "full"
+    mode = "full"
+    createtxtfiles(mode, nlp)
+
+def read():
+    f = open(path_csv, newline='')
+    test_csv = csv.DictReader(f, delimiter=',') #wichtig: gebe hiermit Trennzeichen in csv an
+    return test_csv
+
+def write(input, name):
+    f = open(path + name + ".txt", "w")
+    f.write(str(input))
+    f.close()
+
+def tokenize_word(nlp):
+    test_csv = read()
+
+    for item in test_csv:
+        doc = nlp(item['content'])
+
+        for sentence in doc.sents:
+            for word in sentence:
+                list_word.append(str(word))
+
+    write(list_word, "word")
+    return
+
+def tokenize_sentence(nlp):
+    test_csv = read()
+
+    for item in test_csv:
+        doc = nlp(item['content'])
+
+        for sentence in doc.sents:
+            list_sentence.append(sentence)
+
+    write(list_sentence, "sentence")
+    return
+
+def createtxtfiles(mode, nlp):
+    data = read()
+    count = 0
+
+    #erstelle Datei für jeden Satz
+    if mode == "sentence":
+        for item in data:
+            doc = nlp(item['content'])
+
+            for sentence in doc.sents:
+                f = open(path_folder + str(count) + ".txt", "w")
+                f.write(str(sentence))
+                f.close()
+                count = count + 1
+
+    # erstelle Datei für jedes Item
+    elif mode == "full":
+        for item in data:
+            f = open(path_folder + str(count) + ".txt", "w")
+            f.write(str(item))
+            f.close()
+            count = count + 1
+
+    return
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+#Steinbruch
+def pandasread():
+    data = pandas.read_csv(path_csv)
+    #temp.to_csv('temp_neu') #ein dict als csv speichern
+    # item.update({'processed': temp})
+
+    return data
+
+
+
