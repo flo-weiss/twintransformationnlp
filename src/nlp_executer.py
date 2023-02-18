487ee1dad86df13be92602fde1f1cdd3d70471ed
flo-weiss <florian.weiss@fim-rc.de>
2023-02-18 19:18:19 +0100

    local implementation instead of collab notebook

--- src/nlp_executer.py
+++ src/nlp_executer.py
@@ -22,16 +22,24 @@ AUTOTUNE = tf.data.AUTOTUNE
 batch_size = 32
 seed = 42
 
-#url = 'https://drive.google.com/file/d/1ldHGaMOxB_-HhTYooWgJaJY-571Rp9sv/view?usp=share_link'
-#url = 'https://github.com/flo-weiss/twintransformationnlp/blob/flo/bertintegration/resources/dt2.tar.xz'
+#url = 'https://raw.githubusercontent.com/flo-weiss/twintransformationnlp/flo/bertintegration/resources/dt2.tar.xz'
+url = 'https://raw.githubusercontent.com/flo-weiss/twintransformationnlp/flo/bertintegration/resources/dt.zip'
 
-
-dataset = tf.keras.utils.get_file('dt2.tar.xz', origin=url,
-                                  untar=True, cache_dir='.',
+dataset = tf.keras.utils.get_file('dt.zip',origin=url,
+                                  untar=True, archive_format="zip",cache_dir='.',
                                   cache_subdir='')
 
+dataset_dir = os.path.join(os.path.dirname(dataset), 'dt')
+
+print(os.listdir(dataset_dir))
+print(os.scandir(dataset_dir))
+
+train_dir = os.path.join(dataset_dir, 'train')
+
+print(os.listdir(train_dir))
+
 raw_train_ds = tf.keras.utils.text_dataset_from_directory(
-    'dt/train',
+    'train',
     batch_size=batch_size,
     validation_split=0.2,
     subset='training',
